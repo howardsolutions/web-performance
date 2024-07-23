084e25ad546fd837cae227ffde289b6f14cfe9a0
thinhphungdev <phungthinh0946519356@gmail.com>
2024-07-23 11:59:56 +0700

    performance: using fastDOM

--- javascript-performance/rendering/moving-boxes/script.js
+++ javascript-performance/rendering/moving-boxes/script.js
@@ -5,6 +5,8 @@ const {
 
  const elements = Array.from(document.querySelectorAll('.element'));
 
+
+ // 1) Seperate reading and writing 
 //  registerNextClick(function (timestamp) {
 //   const nextPositions = elements.map(element => {
 //     const top = element.offsetTop;
@@ -19,13 +21,29 @@ const {
 //   });
 //  });
 
- // Using `requestAnimationFrame`
+ // 2) Using `requestAnimationFrame`
+
+//  registerNextClick(function (timestamp) {
+//   elements.forEach((element, index) => {
+//     const top = element.offsetTop;
+//     const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+
+//     requestAnimationFrame(() => element.style.transform = `translateX(${nextPosition}px)`)
+//   });
+//  });
+
+
+// 3) Using FastDOM
 
  registerNextClick(function (timestamp) {
   elements.forEach((element, index) => {
-    const top = element.offsetTop;
-    const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
+    fastdom.measure(() => {
+      const top = element.offsetTop;
+      const nextPosition = +((Math.sin(top + timestamp/1000) + 1) / 2 * containerWidth);
 
-    requestAnimationFrame(() => element.style.transform = `translateX(${nextPosition}px)`)
+      fastdom.mutate(() => {
+        element.style.transform = `translateX(${nextPosition}px)`
+      })
+    })
   });
  });
